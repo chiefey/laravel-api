c896bb9fb001d7d90f9d5646fe69665ab7d8cbac
Brian Gutowski <brian.gutowski@stacksports.com>
2022-02-23 04:24:14 +0000

    begin extending make:controller command

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -4,33 +4,107 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     /**
-     * @OA\Info(
-     *   title="Laravel API",
-     *   version="1.0.0"
-     * )
+     * UserController constructor.
      */
+    public function __construct()
+    {
+        $this->authorizeResource(User::class, 'user');
+    }
 
     /**
      * Display a listing of the resource.
      *
-     * @OA\Get(
-     *      path="/v1/user",
-     *      tags={"User"},
-     *      security={
-     *          {"passport": {}},
-     *      },
-     *      @OA\Response(response="200", description="An example resource")
-     * )
-     * 
      * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * @OA\Get(
+     *      path="/{{ version }}/users",
+     *      operationId="getUserList",
+     *      tags={"User"},
+     *      summary="Get list of User",
+     *      description="Returns list of User",
+     *      security={{"passport": {}}},
+     *      @OA\Parameter(
+     *          ref="#/components/parameters/page"
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(
+     *                      property="data",
+     *                      type="array",
+     *                      @OA\Items(
+     *                          ref="#/components/schemas/User"
+     *                      )
+     *                  ),
+     *                  @OA\Property(
+     *                      property="links",
+     *                      type="object",
+     *                      @OA\Property(property="first", type="string", example=L5_SWAGGER_CONST_USER_PAGE),
+     *                      @OA\Property(property="last", type="string", example=L5_SWAGGER_CONST_USER_PAGE),
+     *                      @OA\Property(property="prev", type="integer", example="null"),
+     *                      @OA\Property(property="next", type="integer", example="null"),
+     *                  ),
+     *                  @OA\Property(
+     *                      property="meta",
+     *                      type="object",
+     *                      @OA\Property(property="currentPage", type="integer", example="1"),
+     *                      @OA\Property(property="from", type="integer", example="1"),
+     *                      @OA\Property(property="lastPage", type="integer", example="1"),
+     *                      @OA\Property(
+     *                          property="links",
+     *                          type="array",
+     *                          @OA\Items(
+     *                              type="object"
+     *                          ),
+     *                          example={
+     *                              {
+     *                                  "url": null,
+     *                                  "label": "&laquo; Previous",
+     *                                  "active": false
+     *                              },
+     *                              {
+     *                                  "url": L5_SWAGGER_CONST_USER_PAGE,
+     *                                  "label": "1",
+     *                                  "active": true
+     *                              },
+     *                              {
+     *                                  "url": null,
+     *                                  "label": "Next &raquo;",
+     *                                  "active": false
+     *                              }
+     *                          }
+     *                      ),
+     *                      @OA\Property(property="path", type="string", example=L5_SWAGGER_CONST_USER_PATH),
+     *                      @OA\Property(property="perPage", type="integer", example="10"),
+     *                      @OA\Property(property="to", type="integer", example="1"),
+     *                      @OA\Property(property="total", type="integer", example="1"),
+     *                  ),
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          ref="#/components/responses/401"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          ref="#/components/responses/403"
+     *      )
+     * )
      */
     public function index()
     {
-        return 'UserController index v1';
+        return User::all();
     }
 
     /**
